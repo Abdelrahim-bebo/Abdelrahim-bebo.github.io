c9eb343a5ff537f9fa4fceee6c9e0d4e4b54439d
Abdelrahim-bebo <abdelrahimbebo@gmail.com>
2025-12-21 15:30:58 +0200

    Fix CRA entry files (public/index.html, src/index.js) and fetch script

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+// ...optional: import "./index.css"; (only if you have it)
+
+ReactDOM.render(<App />, document.getElementById("root"));
